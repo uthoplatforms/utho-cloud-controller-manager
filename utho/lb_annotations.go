d76216991e7811f59ad92c4738bdab04213be134
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-12-16 17:22:07 +0200

    add default annotaion

---
+++ utho/lb_annotations.go
@@ -0,0 +1,10 @@
+package utho
+
+const (
+	// annoUthoLoadBalancerLabel is used to set custom labels for load balancers
+	annoUthoLoadBalancerLabel = "service.beta.kubernetes.io/utho-loadbalancer-label"
+
+	// annoUthoLoadBalancerID is used to identify individual Utho load balancers, this is managed by the CCM
+	annoUthoLoadBalancerID = "service.beta.kubernetes.io/utho-loadbalancer-id"
+
+)

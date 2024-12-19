278ebcafa26301451c10016acfad2d5724a428aa
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-12-19 21:44:57 +0200

    fix typo

--- utho/instancesv2.go
+++ utho/instancesv2.go
@@ -144,7 +144,7 @@ func (i *instancesv2) nodeInstanceAddresses(instance *utho.WorkerNode) ([]v1.Nod
 	}
 
 	addresses = append(addresses,
-		v1.NodeAddress{Type: v1.NodeInternalIP, Address: instance.Ip},
+		v1.NodeAddress{Type: v1.NodeInternalIP, Address: instance.PrivateNetwork.Ip},
 		// v1.NodeAddress{Type: v1.NodeExternalIP, Address: instance.PrivateNetwork.Ip},
 	)
 
